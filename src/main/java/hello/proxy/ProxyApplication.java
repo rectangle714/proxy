19295414ce0f925911c03fc5645e9594c26cf85a
rectangle714@naver.com <rectangle714@naver.com>
2023-03-08 08:24:51 +0900

    20230308 오전 공부 커밋v2

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
+//@Import(AppV1Config.class)
 @Import(AppV1Config.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
