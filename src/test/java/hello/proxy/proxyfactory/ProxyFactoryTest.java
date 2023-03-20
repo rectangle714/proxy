6f8b817c78d42e32205d67d067f4242d52878209
rectangle714@naver.com <rectangle714@naver.com>
2023-03-21 08:28:59 +0900

    20230321 오전 공부 커밋

---
+++ src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
@@ -0,0 +1,13 @@
+package hello.proxy.proxyfactory;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ProxyFactoryTest {
+
+    @Test
+    @DisplayName("인터페이스가 존재하면 JDK 동적 프록시 사용")
+    void interfaceProxy() {
+
+    }
+}
